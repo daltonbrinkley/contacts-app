f4a77393521c97190429f222f5eae393444d3aad
daltonbrinkley <dbrinkles@gmail.com>
2018-03-11 11:06:31 -0500

    Added the 5 RESTful actions to my app

---
+++ app/controllers/v1/contacts_controller.rb
@@ -0,0 +1,71 @@
+class V1::ContactsController < ApplicationController
+  def first_contact_method
+    contact = Contact.first
+    render json: {
+    first_name: contact.first_name,
+    last_name: contact.last_name,
+    email: contact.email,
+    phone_number: contact.phone_number
+    }
+  end
+
+  def second_contact_method
+    contact = Contact.second
+    render json: {
+    first_name: contact.first_name,
+    last_name: contact.last_name,
+    email: contact.email,
+    phone_number: contact.phone_number
+    }
+  end
+
+  def third_contact_method
+    contact = Contact.third
+    render json: {
+    first_name: contact.first_name,
+    last_name: contact.last_name,
+    email: contact.email,
+    phone_number: contact.phone_number
+    }
+  end
+
+  def index
+    contacts = Contact.all
+    render json: contacts.as_json
+  end
+
+  def create
+    contact = Contact.new(
+      first_name: params["input_first_name"],
+      last_name: params["input_last_name"],
+      email: params["input_email"],
+      phone_number: params["input_phone_number"]
+      )
+    contact.save
+    render json: contact.as_json
+  end
+
+  def show
+    contact_id = params["id"]
+    contact = Contact.find_by(id: contact_id)
+    render json: contact.as_json
+  end
+
+  def update
+    contact = Contact.find_by(id: params[:id])
+    contact.first_name = params["input_first_name"] || contact.first_name
+    contact.last_name = params["input_last_name"] || contact.last_name
+    contact.email = params["input_email"] || contact.email
+    contact.phone_number = params["input_phone_number"] || contact.phone_number
+    contact.save
+    render json: contact.as_json
+  end
+
+  def destroy
+    contact_id = params["id"]
+    contact = Contact.find_by(id: contact_id)
+    contact.destroy
+    render json: {message: "Contact deleted!"}
+  end
+
+end
--- frontend.rb
+++ frontend.rb
@@ -6,18 +6,74 @@ puts "Choose an option"
 puts "[1] First Contact"
 puts "[2] Second Contact"
 puts "[3] Third Contact"
+puts "[4] ALL Contacts"
+puts "[5] Create a new contact!" 
+puts "[6] Find Contact by ID"
+puts "[7] Update a contact!"
+puts "[8] Delete a contact!"
 
 input_option = gets.chomp
 if input_option == "1"
-  response = Unirest.get("http://localhost:3000//first_contact_url")
+  response = Unirest.get("http://localhost:3000/v1/first_contact_url")
   contact = response.body
   puts "The info for Contact 1 is: #{contact}"
 elsif input_option == "2"
-  response = Unirest.get("http://localhost:3000/second_contact_url")
+  response = Unirest.get("http://localhost:3000/v1/second_contact_url")
   contact = response.body
   p contact
 elsif input_option == "3"
-  response = Unirest.get("http://localhost:3000/third_contact_url")
+  response = Unirest.get("http://localhost:3000/v1/third_contact_url")
   contact = response.body
   puts contact
-end
+elsif input_option == "4"
+  response = Unirest.get("http://localhost:3000/v1/contacts")
+  contacts = response.body
+  puts contacts
+elsif input_option == "5"
+  params = {}
+  print "Enter contact FIRST name: "
+  params["input_first_name"] = gets.chomp
+  print "Enter contact LAST name: "
+  params["input_last_name"] = gets.chomp
+  print "Enter contact EMAIL: "
+  params["input_email"] = gets.chomp
+  print "Enter contact PHONE NUMBER: "
+  params["input_phone_number"] = gets.chomp
+
+  response = Unirest.post("http://localhost:3000/v1/contacts", parameters: params)
+  contacts = response.body
+  puts JSON.pretty_generate(contacts)
+  
+elsif input_option == "6"
+  print "Please enter contact ID: "
+  id = gets.chomp
+  response = Unirest.get("http://localhost:3000/v1/contacts/#{id}")
+  contact = response.body
+  puts JSON.pretty_generate(contact)
+
+elsif input_option == "7"
+  print "Please enter contact ID: "
+  id = gets.chomp
+  response = Unirest.get("http://localhost:3000/v1/contacts/#{id}")
+  contact = response.body
+  params = {}
+  print "FIRST name (#{contact["first_name"]}): "
+  params["input_first_name"] = gets.chomp
+  print "LAST name (#{contact["last_name"]}): "
+  params["input_last_name"] = gets.chomp
+  print "EMAIL (#{contact["email"]}): "
+  params["input_email"] = gets.chomp
+  print "PHONE NUMBER (#{contact["phone_number"]}): "
+  params["input_phone_number"] = gets.chomp
+  params.delete_if {|_key, value| value.empty? }
+
+  response = Unirest.patch("http://localhost:3000/v1/contacts/#{id}", parameters: params)
+  updated_contact = response.body
+  puts JSON.pretty_generate(updated_contact)
+elsif input_option = "8"
+  print "Enter the contact ID of which you would like to delete: "
+  contact_id = gets.chomp
+  response = Unirest.delete("http://localhost:3000/v1/contacts/#{contact_id}")
+  body = response.body
+  puts JSON.pretty_generate(body)
+end 
